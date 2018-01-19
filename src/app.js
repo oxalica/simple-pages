42246e942fc9b543d56caf68101bbbb0f24f6215
uHOOCCOOHu <hooccooh1896@gmail.com>
2018-01-19 15:13:52 +0800

    Impl main logic. Fix bugs causing high CPU usage.

--- src/app.js
+++ src/app.js
@@ -9,22 +9,95 @@
         password: '',
         repo: '',
         branch: 'master',
+        logining: false,
+        errorMsg: null,
       };
+    },
+    methods: {
+      login: function() {
+        if(this.logining)
+          throw new TypeError('Double login');
+        this.logining = true;
+        this.errorMsg = null;
+
+        let loginCfg = {
+          username: this.username,
+          password: this.password,
+          repo: this.repo,
+          branch: this.branch,
+        };
+        GhBlog.load(loginCfg)
+              .then(ghBlog => {
+                if(!ghBlog.available)
+                  throw new Error('Not initialized by simple-pages');
+                this.$emit('success', ghBlog);
+              })
+              .catch(err => {
+                this.errorMsg = err.message;
+                console.error(err);
+              })
+              .then(() => this.logining = false);
+      },
     },
   });
 
   Vue.component('mainPage', {
     template: '#templ-main-page',
+    props: {
+      ghBlogIn: GhBlog,
+    },
     data: function() {
       return {
+        ghBlog: this.ghBlogIn,
         curArticle: {
-          filename: '',
+          name: '',
           title: '',
-          pubtime: '',
-          content: '',
-          rendered: '',
+          isoPubtime: '',
+          tags: '', // String
+          source: '',
         },
+        waiting: false,
+        errorMsg: null,
       };
+    },
+    methods: {
+      saveCurrentArticle: function() {
+        if(this.waiting)
+          throw new TypeError('Save when waiting');
+        this.waiting = true;
+        this.errorMsg = null;
+
+        let source = this.curArticle.source;
+        let match = /^([\s\S]*?)--+more--+([\s\S]*)$/.exec(source);
+        let article = Object.assign({}, this.curArticle);
+        article.tags = article.tags.split(/\s+/);
+        article.renderedBrief = match ? marked(match[1]) : '';
+        article.rendered = marked(match ? match[1] + match[2] : source);
+        this.ghBlog.writeArticle(article, 'Save')
+            .then(() => {
+              this.waiting = false;
+              window.alert('Saved!')
+            })
+            .catch(err => {
+              this.waiting = false;
+              this.errorMsg = err;
+            });
+      },
+      loadArticle: function() {
+        if(this.waiting)
+          throw new TypeError('Load when waiting');
+        this.waiting = true;
+        this.errorMsg = null;
+        this.ghBlog.readArticle(this.curArticle.name)
+            .then(article => {
+              if(article === null)
+                throw new Error('Article not found');
+              article.tags = article.tags.join(' ');
+              this.curArticle = article;
+            })
+            .catch(err => this.errorMsg = err.message)
+            .then(() => this.waiting = false);
+      },
     },
   });
 
@@ -32,6 +105,8 @@
     template: '#templ-md-editor',
     props: {
       'value': String,
+      'readonly': Boolean,
+      'disabled': Boolean,
     },
     data: function() {
       return {
@@ -39,18 +114,16 @@
       };
     },
     computed: {
-      content: {
+      source: {
         get: function() {
           return this.value;
         },
-        set: function(newContent) {
-          this.$emit('input', newContent);
+        set: function(newSource) {
+          this.$emit('input', newSource);
         },
       },
       rendered: function() {
-        let r = marked(this.content);
-        this.$emit('update-rendered', r);
-        return r;
+        return marked(this.source);
       },
     },
     watch: {
@@ -60,8 +133,16 @@
     },
   });
 
-  new Vue({
+  window.root = new Vue({
     el: '#app',
+    data: {
+      ghBlog: null,
+    },
+    methods: {
+      loginSuccess: function(ghBlog) {
+        this.ghBlog = ghBlog;
+      },
+    },
   });
 
 })();
